2fb36c686d4ceaf91bc526f321ff211581772566
vacit <vacitabdulrazak@gmail.com>
2020-02-20 12:10:25 +0300

    Updates

---
+++ precache-manifest.6c2565531111ad9f5be27211750895ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b025003a40127a99f4280bd509c9763",
+    "url": "/practicing-react/index.html"
+  },
+  {
+    "revision": "11a5a4eefb4db5164441",
+    "url": "/practicing-react/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "5148151d85c5dd4c4dec",
+    "url": "/practicing-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "11a5a4eefb4db5164441",
+    "url": "/practicing-react/static/js/2.1876de5e.chunk.js"
+  },
+  {
+    "revision": "5148151d85c5dd4c4dec",
+    "url": "/practicing-react/static/js/main.a930851a.chunk.js"
+  },
+  {
+    "revision": "82aaa022c0eeeb33a1b5",
+    "url": "/practicing-react/static/js/runtime-main.1131580e.js"
+  }
+]);
